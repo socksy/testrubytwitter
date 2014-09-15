0c6a84fff799c89b6809703ec607133c7fa189b5
Ben Lovell <ben.j.lovell@gmail.com>
2014-09-15 09:41:04 +0100

    Pagination of a user's tweets now works

---
+++ see.rb
@@ -0,0 +1,42 @@
+require 'rubygems'
+require 'twitter'
+
+if !ENV["TEST_SOURCED_RET3453"] then
+  abort "You need to source authdetails.sh before you can run this program!"
+end
+
+client = Twitter::REST::Client.new do |config|
+  config.consumer_key        = ENV["CONSUMER_KEY"]
+  config.consumer_secret     = ENV["CONSUMER_SECRET"]
+  config.access_token        = ENV["ACCESS_TOKEN"]
+  config.access_token_secret = ENV["ACCESS_SECRET"]
+end
+
+print "Which user's timeline do you want to see? "
+user = gets.chomp
+
+NUMBER = 10
+puts "First #{NUMBER} tweets"
+last_id = nil #for pagination
+while true do
+  options = {count: NUMBER, include_rts: true}
+  #only care about this on sequential page loads
+  options[:max_id] = (last_id-1) unless last_id.nil? #max_id because we're going backwards
+
+  client.user_timeline("socksy", options)
+  .each do |tweet| 
+    puts "#{tweet.id}:\n #{tweet.text}"
+    puts "-"*20
+    
+    if last_id.nil? then
+      last_id = tweet.id
+    else
+      last_id = (tweet.id < last_id) ? tweet.id : last_id
+    end
+  end
+
+  print "Next #{NUMBER}? (y/n) "
+  if gets.chomp != 'y' then
+    break
+  end
+end
